66576ed9c21bf899069cdcad8d67d6c07b4b177b
Muhammad Rashid <muhammadrashid172002@gmail.com>
2025-07-30 21:45:32 -0700

    Updated UI screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:expanse_tracker_app/Screens/Splash_Screen/Splash_Screen.dart';
+import 'package:expanse_tracker_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
@@ -10,22 +12,27 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
+  // Handle background notification
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  tz.initializeTimeZones();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('@mipmap/ic_launcher');
+  if (!kIsWeb) {
+    // Android/iOS only - Not for Web
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  final InitializationSettings initializationSettings = InitializationSettings(
-    android: initializationSettingsAndroid,
-  );
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
 
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    tz.initializeTimeZones();
+  }
 
   runApp(const MyApp());
 }
@@ -33,7 +40,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -46,22 +52,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA5TofY-8ADGZF6uO1F-DohFgEB6BSGSZY",
-//   authDomain: "artha-b88ab.firebaseapp.com",
-//   projectId: "artha-b88ab",
-//   storageBucket: "artha-b88ab.firebasestorage.app",
-//   messagingSenderId: "155227403448",
-//   appId: "1:155227403448:web:36809fadbc8cb38e8ef04d"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
